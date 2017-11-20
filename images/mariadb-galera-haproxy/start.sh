06d2aa83a1d3b34f8a926a0877341feea58bbeec
Sean Hignett <sean@intelligent-design.ca>
2017-11-20 15:02:54 -0700

    tweaks in support of rolling db upgrades under heavier load

--- images/mariadb-galera-haproxy/start.sh
+++ images/mariadb-galera-haproxy/start.sh
@@ -6,10 +6,12 @@ echo "Setting up HA PROXY..."
 printf "HAPROXY_GALERA_NODE_1=${HAPROXY_GALERA_NODE_1}\n" >> /etc/default/haproxy
 printf "HAPROXY_GALERA_NODE_2=${HAPROXY_GALERA_NODE_2}\n" >> /etc/default/haproxy
 printf "HAPROXY_GALERA_NODE_3=${HAPROXY_GALERA_NODE_3}\n" >> /etc/default/haproxy
+printf "HAPROXY_GALERA_TIMEOUT=${HAPROXY_GALERA_TIMEOUT}\n" >> /etc/default/haproxy
 
 echo "NODE_1: ${HAPROXY_GALERA_NODE_1}"
 echo "NODE_2: ${HAPROXY_GALERA_NODE_2}"
 echo "NODE_3: ${HAPROXY_GALERA_NODE_3}"
+echo "TIMEOUT: ${HAPROXY_GALERA_TIMEOUT}"
 
 echo "Starting HA PROXY..."
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg -V
